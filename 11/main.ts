6d46440aac67fd57f67bfbcb74c12c5f644342b0
Jeff Schiller <codedread@gmail.com>
2024-12-28 09:43:54 +0900

    Day 11 Part 2 solution

--- 11/main.ts
+++ 11/main.ts
@@ -71,6 +71,8 @@ class LinkedList<T> {
   }
 }
 
+const map: Map<number, number> = new Map();
+
 /**
  * - If the stone is engraved with the number 0, it is replaced by a stone
  *   engraved with the number 1.
@@ -86,6 +88,8 @@ function blink(list: LinkedList<number>) {
   let n: ListItem<number>|undefined = list.head;
   let next = list.head?.next;
   while (n) {
+    if (!map.has(n.val)) map.set(n.val, 0);
+    map.set(n.val, map.get(n.val)! + 1);
     const numStr = `${n.val}`;
     const numDigits = numStr.length;
     if (n.val === 0) {
@@ -116,6 +120,84 @@ async function main1(filename: string) {
     blink(list);
   }
   console.log(`Number of stones = ${list.size()}`);
+
+  const allKeys = Array.from(map.keys());
+  console.log(`# of unique stones = ${allKeys.length}`);
+  const repeats = [];
+  for (const [key, val] of map) {
+    if (val > 1) {
+      // console.log(`${key} => ${val}`);
+      repeats.push([key,val]);
+    }
+  }
+  console.log(`# of stones that repeated = ${repeats.length}`);
 }
 
-main1('./11/input.txt');
+/**
+ * The above solution is memory intensive as it's storing each stone through all
+ * blink operations in a data structure. Going higher than ~40 links takes an
+ * extremely long amount of time and eventually runs out of memory. It's also a
+ * lot of code.
+ *
+ * An analysis of the first part solution yields that even though we had 228668
+ * stones for part 1, we only had 840 unique stones. And 214 of those repeated
+ * more than once.
+ *
+ * This suggests that we could memoize each unique stone's blinks in a map so
+ * that we don't have to store the sub-lists at all, just the link results as we
+ * recurse.
+ */
+
+/**
+ * This map stores the result of blinking a stone n times. The key is a
+ * concatenated string of 'stone-blinkTimes'. i.e. blinking a "2024" stone 4 times
+ * would use a key "2024-4" to store the resulting # of stones produced.
+ */
+const blinkMemoizer: Map<string, number> = new Map();
+const memoKey = (stone: number, n: number) => `${stone}-${n}`;
+
+/** Blinks a single stone n times, returning the resulting number of stones. */
+function blinkStone(stone: number, n: number): number {
+  const key = memoKey(stone, n);
+  if (blinkMemoizer.has(key)) {
+    return blinkMemoizer.get(key)!;
+  }
+
+  const nextStones: number[] = [];
+  const numStr = `${stone}`;
+  const numDigits = numStr.length;
+
+  if (stone === 0) {
+    nextStones.push(1);
+  } else if (numDigits % 2 === 0) {
+    const left = parseInt(numStr.substring(0, numDigits / 2));
+    const right = parseInt(numStr.substring(numDigits / 2));
+    nextStones.push(left, right);
+  } else {
+    nextStones.push(stone * 2024);
+  }
+
+  if (n > 1) {
+    let sumStones = 0;
+    for (const nextStone of nextStones) {
+      const sumNextStone = blinkStone(nextStone, n-1);
+      blinkMemoizer.set(memoKey(nextStone, n-1), sumNextStone);
+      sumStones += sumNextStone;
+    }
+    return sumStones;
+  }
+  return nextStones.length;
+}
+
+async function main2(filename: string) {
+  const stones = (await Deno.readTextFile(filename))
+      .split(' ')
+      .map(s => parseInt(s));
+  let numStones = 0;
+  for (const stone of stones) {
+    numStones += blinkStone(stone, 75);
+  }
+  console.log(`# of stones = ${numStones}`);
+}
+
+main2('./11/input.txt');
