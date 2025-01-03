73bffc1deba1ba71867173f2f6fc14664fd4f36d
Jeff Schiller <codedread@gmail.com>
2025-01-02 23:19:20 -0800

    Day 14 Part 2 solution

--- 14/main.ts
+++ 14/main.ts
@@ -35,22 +35,7 @@ class Space {
     }
   }
 
-  /** Returns the safety factor. */
-  runRobots(numSeconds: number): number {
-    const pts: Point[] = [];
-    for (const robot of this.robots) {
-      const newPos = robot.start.addition(
-        new Point(robot.velocity.x * numSeconds,
-          robot.velocity.y * numSeconds));
-      // Do teleportation.
-      newPos.x %= this.W;
-      newPos.y %= this.H;
-      if (newPos.x < 0) { newPos.x += this.W; }
-      if (newPos.y < 0) { newPos.y += this.H; }
-
-      pts.push(newPos);
-    }
-
+  getSafetyFactor(pts: Point[]): number {
     const quadrantX = Math.floor(this.W / 2);
     const quadrantY = Math.floor(this.H / 2);
     let nw = 0, ne = 0, sw = 0, se = 0;
@@ -67,13 +52,81 @@ class Space {
     }
     return nw * sw * ne * se;
   }
+
+  /** Returns the points of the robots after numSeconds time. */
+  runRobots(numSeconds: number): Point[] {
+    const pts: Point[] = [];
+    for (const robot of this.robots) {
+      const newPos = robot.start.addition(
+        new Point(robot.velocity.x * numSeconds,
+          robot.velocity.y * numSeconds));
+      // Do teleportation.
+      newPos.x %= this.W;
+      newPos.y %= this.H;
+      if (newPos.x < 0) { newPos.x += this.W; }
+      if (newPos.y < 0) { newPos.y += this.H; }
+
+      pts.push(newPos);
+    }
+    return pts;
+  }
+
+  visualize(pts: Point[]) {
+    const grid: number[][] = [];
+    for (let y = 0; y < this.H; ++y) {
+      grid.push(new Array(this.W).fill(0));
+    }
+
+    for (const pt of pts) {
+      grid[pt.y][pt.x]++;
+    }
+
+    for (const row of grid) {
+      console.log(row.map(v => v === 0 ? '.' : `${v}`).join());
+    }
+  }
 }
 
 async function main1(filename: string, W: number, H: number, numSeconds: number) {
   const lines = (await Deno.readTextFile(filename)).split(/\r?\n/);
   const space = new Space(lines, W, H);
-  console.log(`Safety factor = ${space.runRobots(numSeconds)}`);
+  const safetyFactor = space.getSafetyFactor(space.runRobots(numSeconds));
+  console.log(`Safety factor = ${safetyFactor}`);
+}
+
+/**
+ * As we iterate over first 1000 seconds, we see box-like pictures at various
+ * points in time: 13, 79, 114, 182, 215, 285, 316, 388, ...
+ * The odd ones start at 13 and add 101 (W) each time.
+ * The even ones start at 79 and add 103 (H) each time.
+ */
+async function main2(filename: string, W: number, H: number, numSeconds: number) {
+  const lines = (await Deno.readTextFile(filename)).split(/\r?\n/);
+  const space = new Space(lines, W, H);
+  for (let t = 0; t < 1000000; ++t) {
+    let foundMultiple = false;
+    if (t >= 13 && (t - 13) % 101 === 0) {
+      foundMultiple = true;
+    }
+    if (t >= 79 && (t - 79) % 103 === 0) {
+      foundMultiple = true;
+    }
+
+    if (foundMultiple) {
+      const pts = space.runRobots(t);
+      console.clear();
+      space.visualize(pts);
+      console.log(`${t}: Press enter`);
+      const arr = new Uint8Array(100);
+      await Deno.stdin.readSync(arr);
+      const input = new TextDecoder().decode(arr);
+      if (input[0] === 'p') {
+        if (t > 0) t -= 2;
+        else t--;
+      }
+    }
+  }
 }
 
 //main1(`./14/input_test_1.txt`, 11, 7, 100);
-main1(`./14/input.txt`, 101, 103, 100);
+main2(`./14/input.txt`, 101, 103, 100);
